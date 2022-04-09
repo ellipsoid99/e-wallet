2c398b265234de01852fd4c987d962197d34bd92
ellipsoid <rijusohambiswas@gmail.com>
2022-04-09 18:23:37 +0530

    backend

--- routes/api/users.js
+++ routes/api/users.js
@@ -86,54 +86,53 @@ router.post("/register",async  (req, res) => {
 router.post("/login", (req, res) => {
   // Form validation
 
-//   const { errors, isValid } = validateLoginInput(req.body);
+  const { errors, isValid } = validateLoginInput(req.body);
 
-//   // Check validation
-//   if (!isValid) {
-//     return res.status(400).json(errors);
-//   }
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
 
-//   const email = req.body.email;
-//   const password = req.body.password;
+  const accountnumber = req.body.accountnumber;
+  const password = req.body.password;
 
-//   // Find user by email
-//   User.findOne({ email }).then(user => {
-//     // Check if user exists
-//     if (!user) {
-//       return res.status(404).json({ emailnotfound: "Email not found" });
-//     }
+  // Find user by email
+  User.findOne({ accountnumber }).then(user => {
+    // Check if user exists
+    if (!user) {
+      return res.status(404).json({ emailnotfound: "User is not registered" });
+    }
 
-//     // Check password
-//     bcrypt.compare(password, user.password).then(isMatch => {
-//       if (isMatch) {
-//         // User matched
-//         // Create JWT Payload
-//         const payload = {
-//           id: user.id,
-//           name: user.name
-//         };
+    // Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        // User matched
+        // Create JWT Payload
+        const payload = {
+          id: user.id,
+          name: user.firstname
+        };
 
-//         // Sign token
-//         jwt.sign(
-//           payload,
-//           keys.secretOrKey,
-//           {
-//             expiresIn: 31556926 // 1 year in seconds
-//           },
-//           (err, token) => {
-//             res.json({
-//               success: true,
-//               token: "Bearer " + token
-//             });
-//           }
-//         );
-//       } else {
-//         return res
-//           .status(400)
-//           .json({ passwordincorrect: "Password incorrect" });
-//       }
-//     });
-//   });
-// });
-})
+        // Sign token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          {
+            expiresIn: 31556926 // 1 year in seconds
+          },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
+      } else {
+        return res
+          .status(400)
+          .json({ passwordincorrect: "Password incorrect" });
+      }
+    });
+  });
+});
 module.exports = router;
--- validation/login.js
+++ validation/login.js
@@ -5,14 +5,14 @@ module.exports = function validateLoginInput(data) {
   let errors = {};
 
   // Convert empty fields to an empty string so we can use validator functions
-  data.email = !isEmpty(data.email) ? data.email : "";
+  data.accountnumber = !isEmpty(data.accountnumber) ? data.accountnumber : "";
   data.password = !isEmpty(data.password) ? data.password : "";
 
   // Email checks
-  if (Validator.isEmpty(data.email)) {
-    errors.email = "Email field is required";
-  } else if (!Validator.isEmail(data.email)) {
-    errors.email = "Email is invalid";
+  if (Validator.isEmpty(data.accountnumber)) {
+    errors.accountnumber = "AccountNumber field is required";
+  } else if (!Validator.isEmail(data.accountnumber)) {
+    errors.accountnumber = "AccountNumber is invalid";
   }
   // Password checks
   if (Validator.isEmpty(data.password)) {
