b184d27623f00d1845bbd85cd95c92a07fd30475
ellipsoid <rijusohambiswas@gmail.com>
2022-05-21 18:38:21 +0530

    loginCount api

--- client/src/components/payments/index.js
+++ client/src/components/payments/index.js
@@ -2,13 +2,13 @@ import PaymentSecure from "./PaymentSecure";
 import styles from "./Payment.module.scss";
 
 const PaymentsComponent = () => {
-    return (
-        <div className={`${styles.wrapper} gradientBase`}>
-            <div className={styles.formContainer}>
-                <PaymentSecure />
-            </div>
-        </div>
-    );
+  return (
+    <div className={`${styles.wrapper} gradientBase`}>
+      <div className={styles.formContainer}>
+        <PaymentSecure />
+      </div>
+    </div>
+  );
 };
 
 export default PaymentsComponent;
